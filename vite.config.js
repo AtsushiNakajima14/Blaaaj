59115fe91baacf21d9d07f4457f4b410b178d5cb
gauhsoj <coglatessodms@gmail.com>
2025-01-25 03:57:04 +0000

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import VitePluginFullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+  server: {
+    allowedHosts: true
+  },
   plugins: [
     VitePluginFullReload(['**/*.html'], { log: false }),
   ]
